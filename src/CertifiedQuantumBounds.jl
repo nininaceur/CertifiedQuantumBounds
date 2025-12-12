aa6ecb9cafbce183d0df262b6081905e850850af
nininaceur <naceur.younes@yahoo.de>
2025-12-12 15:39:47 +0100

    Initial commit

---
+++ src/CertifiedQuantumBounds.jl
@@ -0,0 +1,26 @@
+module CertifiedQuantumBounds
+
+using LinearAlgebra
+using NCTSSOS
+using DynamicPolynomials
+using ITensors
+using ITensorMPS
+using QMBCertify
+import DynamicPolynomials: Monomial, Polynomial
+using Arblib
+
+include(joinpath(@__DIR__, "nctssos_cert", "caches.jl"))
+include(joinpath(@__DIR__, "nctssos_cert", "helpers.jl"))
+include(joinpath(@__DIR__, "nctssos_cert", "dense.jl"))
+include(joinpath(@__DIR__, "nctssos_cert", "sparse.jl"))
+include(joinpath(@__DIR__, "qmb_cert", "helpers.jl"))
+include(joinpath(@__DIR__, "qmb_cert", "energy_cert.jl"))
+include(joinpath(@__DIR__, "qmb_cert", "corr_cert.jl"))
+
+export rational_certificate, rational_certificate_sparse
+export certify_qmb, certify_qmb_corr, dmrg_heisenberg_rat
+export clear_caches!       
+export merged_coeffs       
+export symmetrize             
+
+end
